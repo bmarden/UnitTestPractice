c6e2f053dc94f15ef6bd5b02ba811e6e2f963c8b
Ben Marden <benvmarden@gmail.com>
2020-04-21 21:41:11 -0700

    Add second unit test

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -3,20 +3,25 @@
 **/
 
 #include <gtest/gtest.h>
+
 #include "Practice.h"
 
-class PracticeTest : public ::testing::Test
-{
-	protected:
-		PracticeTest(){} //constructor runs before each test
-		virtual ~PracticeTest(){} //destructor cleans up after tests
-		virtual void SetUp(){} //sets up before each test (after constructor)
-		virtual void TearDown(){} //clean up after each test, (before destructor)
+class PracticeTest : public ::testing::Test {
+   protected:
+    PracticeTest() {}           //constructor runs before each test
+    virtual ~PracticeTest() {}  //destructor cleans up after tests
+    virtual void SetUp() {}     //sets up before each test (after constructor)
+    virtual void TearDown() {}  //clean up after each test, (before destructor)
 };
 
-TEST(PracticeTest, is_simple_palindrome)
-{
+TEST(PracticeTest, is_simple_palindrome) {
     Practice obj;
     bool actual = obj.isPalindrome("aa");
     ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, is_not_a_palindrome) {
+    Practice obj;
+    bool actual = obj.isPalindrome("Ba");
+    ASSERT_FALSE(actual);
 }
